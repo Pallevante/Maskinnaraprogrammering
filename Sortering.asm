875830f2ecc47d62cef418394326e13186a89a75
Pallevante <pallevante@live.se>
2014-04-14 17:41:33 +0200

    Identering because fuck logic.

--- Sortering.asm
+++ Sortering.asm
@@ -37,42 +37,43 @@ dbgp:	.asciiz "Doing sort"
 	
 newline: .asciiz "\n"	
 	
-.text						# fuck this fucking piece of fucking goddamn shit
+.text					# fuck this fucking piece of fucking goddamn shit
 				
 main:
-	lw	$s1, datalen			# Add 16 to s1, data limit.
-	la	$s2, 0				# PrintLoop itterator
+	lw	$s1, datalen		# Add 16 to s1, data limit.
+	la	$s2, 0			# PrintLoop itterator
 	
-	la	$s0, data 			# X-Array
-	la	$s3, data 			# Y-Array
+	la	$s0, data 		# X-Array
+	la	$s3, data 		# Y-Array
+
+	li	$s4, 0			# Numb of Y-loops.
+	li	$s6, 0			# Numb of X-loops.
 	
-	li	$s4, 0				# Numb of Y-loops.
-	li	$s6, 0				# Numb of X-loops.
+	# This will use the basic of X and Y.
+	# Y will always be X + 1 until it's out of bounds.
 	
 	
-	
-	
-		# When I started this only me and god knew what I was thinking.
-		# Now only god knows....				
+	# When I started this only me and god knew what I was thinking.
+	# Now only god knows....				
 						
 	
 xLoop:					
-	bge	$s6, $s1, printLoop		# If we have gone through 16 loops.
+	bge	$s6, 16, printLoop	# If we have gone through 16 loops.
 	nop
 	
-	lw	$a0, 0($s0)			# Adds x to address a0.		
+	lw	$a0, 0($s0)		# Adds x to address a0.		
 	nop
 	
 	jal 	yLoop
 	nop
-		# Fucking shit stuff.
+	# Fucking shit stuff.
 		
 		
 yLoop:
 	jal 	moveY
 	nop
 	
-	lw 	$a1, 0($s3)			# Adds y to address a1.
+	lw 	$a1, 0($s3)		# Adds y to address a1.
 	nop 
 	
 	blt 	$a1, $a0, SortValues
@@ -86,30 +87,31 @@ yLoop:
 									
 
 moveX:
-	addi	$s0, $s0, 4			# New x 
+	addi	$s0, $s0, 4		# New x 
 	
-	addi	$s6, $s6, 1			# Uppdates the numb of X loops.
+	addi	$s6, $s6, 1		# Uppdates the numb of X loops.
 	nop
-	addi	$s4, $s6, 0			# Y loop to X loop.
+	addi	$s4, $s6, 0		# Y loop to X loop.
 	
 	j	xLoop
 	nop
 
 
 moveY:
-	addi	$s3, $s3, 4			# New y
+	addi	$s3, $s3, 4		# New y
 	addi	$s4, $s4, 1
-	jr 	$ra				# Back to the Future.
+	jr 	$ra			# Back to the Future.
 	nop
 
 	
 SortValues:		
-	add	$t0, $0, $a0			# Loads the temp address.
+	add	$t0, $0, $a0		# Loads the temp address.
 	add	$a0, $0, $a1
 	add	$a1, $0, $t0		
 	
-	sw	$a0, 0($s3)
-	sw	$a1, 0($s0)
+	sw	$a1, 0($s0)		# Adds the new X-Value to the X-Array
+	sw	$a0, 0($s3)		# Adds the new Y-Value to the Y-Array
+	
 	
 	jr 	$ra
 	nop	
