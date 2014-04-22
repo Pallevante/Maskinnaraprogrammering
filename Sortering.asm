c13ca134faa03ce7d1fccaf25f83ba05e525f6d4
Pallevante <pallevante@live.se>
2014-04-22 18:43:39 +0200

    Fixat allt.

--- Sortering.asm
+++ Sortering.asm
@@ -6,34 +6,12 @@
 # 2014
 
 
-	.data
-datalen:
-	.word	0x0010			# 16
 
-	
-data:
-	.word	0xffff7e81
-	.word	0x00000001
-	.word	0x00000002
-	.word	0xffff0001
-	.word	0x00000000
-	.word	0x00000001
-	.word	0xffffffff
-	.word	0x00000000
-	.word	0xe3456687
-	.word	0xa001aa88
-	.word	0xf0e159ea
-	.word	0x9152137b
-	.word	0xaab385a1
-	.word	0x31093c54
-	.word	0x42102f37
-	.word	0x00ee655b
-	
-	
-newline: .asciiz "\n"	
-	
+
+
 .text					
-				
+			
+												
 main:
 	lw	$s3, datalen		# Add 16 to s1, data limit.
 	la	$s5, 0			# PrintLoop itterator
@@ -126,8 +104,8 @@ printLoop:
 	li	$v0, 1
 	syscall
 	
-	la 	$a0, newline
-	li 	$v0, 4
+	li 	$a0, 10
+	li 	$v0, 11
 	syscall
 	
 	addi 	$s5, $s5, 1		# Adds to the itterator 
